6c4b139a9513f0d38f029455b107ba1c69b98652
thanawat.bcr <thanwat.bcr@gmail.com>
2022-09-02 16:38:40 +0700

    feat: kairos api

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
-const Kairos = require('kairos-api');
 const JSONStream = require('JSONStream');
 const bodyParser = require('body-parser');
 const multipart = require('connect-multiparty');
+const axios = require('axios');
+const kairosAxios = require('axios');
+
+kairosAxios.defaults.headers.common['app_id'] = '68b0c0ea'
+kairosAxios.defaults.headers.common['app_key'] = '3e1615c6719a7b955cb417ba8045f4e1'
+kairosAxios.defaults.headers.common['Content-Type'] = 'application/json'
 
 const app = express();
 app.use(cors());
@@ -14,74 +19,96 @@ app.use(bodyParser.json());
 // Multiparty middleware
 const multipartMiddleware = multipart();
 
-// API Configurations for KAIROS - PASSI
-let kairos_client = new Kairos('68b0c0ea', '3e1615c6719a7b955cb417ba8045f4e1');
-
 // TEST API
 app.get('/test', (req, res) => { console.log("TEST GET!"); return res.send('HELLO TEST'); })
 app.post('/test', (req, res) => { console.log("TEST POST!"); return res.send('HELLO TEST'); })
+app.get('/cat', async (req, res) => {
+    try {
+        const response = await axios.get("https://catfact.ninja/fact")
+        res.json(response.data)
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
 
-// ENROLL FACE INTO GALLRY
-app.post('/enroll', multipartMiddleware, function(req, res) {
+// GET ALL GALLERIES ✅
+app.get('/kairos/galleries', async (req, res) => {
+    console.log('GET ALL GALLERIES 🎒');
+    try {
+        const result = await kairosAxios.post('https://api.kairos.com/gallery/list_all')
+        return res.json(result.data);
+    } catch(err) {
+        console.log(err);
+        return res.json({'status' : false});
+    }
+});
+
+// GET ALL SUBJECTS IN GALLERY ✅
+app.get('/kairos/gallery', async (req, res) => {
+    console.log(`GET ALL SUBJECTS IN ${req.query.gallery_name} GALLERY 👨`);
+    try {
+        const result = await kairosAxios.post('https://api.kairos.com/gallery/view', {
+            gallery_name: req.query.gallery_name
+        })
+        return res.json(result.data);
+    } catch(err) {
+        console.log(err);
+        return res.json({'status' : false});
+    }
+});
+
+// GET ALL SUBJECTS IN GALLERY ✅
+app.get('/kairos/gallery/subject', async (req, res) => {
+    console.log(`GET ALL FACES OF ${req.query.subject_id} 👨`);
+    try {
+        const result = await kairosAxios.post('https://api.kairos.com/gallery/view_subject', {
+            gallery_name: req.query.gallery_name,
+            subject_id: req.query.subject_id
+        })
+        return res.json(result.data);
+    } catch(err) {
+        console.log(err);
+        return res.json({'status' : false});
+    }
+});
+
+// ENROLL FACE BY SUBJECT_ID ✅
+app.post('/kairos/enroll',multipartMiddleware , async (req, res) => {
+    console.log(`ENROLL FACE BY SUBJECT_ID: ${req.body.subject_id} 👨`);
     let base64image = fs.readFileSync(req.files.image.path, 'base64');
     var params = {
         image: base64image,
-        subject_id: req.body.name,
         gallery_name: req.body.gallery_name,
+        subject_id: req.body.subject_id,
     };
-    console.log('POST: Enroll Face');
-    kairos_client.enroll(params).then(function(result) {
-        // console.log('Image Attributes : \n' + JSON.stringify(result.body));
-        return res.json({'status' : true });
-    }).catch(function(err) {
+    try {
+        const result = await kairosAxios.post('https://api.kairos.com/enroll', params)
+        return res.json({'status': true, 'face_id': result.data.face_id});
+    } catch(err) {
         console.log(err);
         return res.json({'status' : false});
-    });
+    }
 });
 
-// VERIFY FACE FROM GALLRY
-app.post('/verify', multipartMiddleware, function(req, res) {
+// VERIFY FACE BY SUBJECT_ID ✅
+app.post('/kairos/verify',multipartMiddleware , async (req, res) => {
+    console.log(`VERIFY FACE BY SUBJECT_ID: ${req.body.subject_id} 👨`);
     let base64image = fs.readFileSync(req.files.image.path, 'base64');
     var params = {
         image: base64image,
-        subject_id: req.body.name,
         gallery_name: req.body.gallery_name,
+        subject_id: req.body.subject_id,
     };
-    console.log('POST: Verify Face', req.body.name);
-    kairos_client.verify(params).then(function(result) {
-        // console.log('Server responded with : \n' + JSON.stringify(result));
-        return res.json(result.body);
-    }).catch(function(err) { 
+    try {
+        const result = await kairosAxios.post('https://api.kairos.com/verify', params)
+        // CHECK AT CONFIDENCE MUST BE GREATER THAN 60%
+        console.log(result.data.images[0].transaction.confidence)
+        return res.json({'status': result.data.images[0].transaction.confidence > 0.6});
+    } catch(err) {
         console.log(err);
         return res.json({'status' : false});
-    });  
-});
-
-// GET SUBJECT FROM GALLERY
-app.get('/kairos/gallery', function(req, res) {
-    var params = {
-        gallery_name: req.query.gallery_name,
-    };
-    console.log('GET: All subjects from', req.query.gallery_name);
-    kairos_client.galleryView(params).then(function(result) {
-        // console.log('Server responded with : \n' + JSON.stringify(result));
-        return res.json(result.body);
-    }).catch(function(err) { 
-        console.log(err);
-        return res.json({'status' : false});
-    });  
-});
-
-// GET ALL GALLERIES
-app.get('/kairos/galleries', function(req, res) {
-    console.log('GET: All galleries');
-    kairos_client.galleryListAll().then(function(result) {
-        // console.log('Server responded with : \n' + JSON.stringify(result));
-        return res.json(result.body);
-    }).catch(function(err) { 
-        console.log(err);
-        return res.json({'status' : false});
-    });  
+    }
 });
 
 
