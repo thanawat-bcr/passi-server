16c166822f4f9cacaa469468d887bb7470701ace
thanawat.bcr <thanwat.bcr@gmail.com>
2022-09-06 19:54:05 +0700

    demo table

---
+++ table/insert-passport.sql
@@ -0,0 +1,16 @@
+INSERT INTO passport (passport_no, name, surname, type, country_code, nationality, date_of_birth, place_of_birth, identification_no, sex, height, date_of_issue, date_of_expiry) 
+VALUES (
+  'AB1325944',
+  'Thanawat',
+  'Benjachatriroj',
+  'P',
+  'THA',
+  'THAI',
+  '15 Oct 2000',
+  'BANGKOK',
+  '1100401055261',
+  'M',
+  '1.8',
+  '16 Mar 2018',
+  '15 Mar 2023'
+)
