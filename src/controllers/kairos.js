af9148052db7f23e4ea6ac8b70f3eca974f353e2
thanawat.bcr <thanwat.bcr@gmail.com>
2022-09-21 16:29:24 +0700

    fix: passport no

--- src/controllers/kairos.js
+++ src/controllers/kairos.js
@@ -74,7 +74,7 @@ async function enroll(req, res, next) {
   };
   try {
       const passports = await knex.select('passport_no').from('passport').whereILike('passport_no', 'PASSI%')
-      const passport_id = `0000${passports.length}`
+      const passport_id = `0000${passports.length + 1}`
       const passport_no = 'PASSI' + passport_id.substring(passport_id.length - 4) 
       // console.log(passport_no)
       let today = new Date().toISOString().split('T')[0]
