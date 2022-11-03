db94b83f31a35650e539703b489b1657e48cedd8
thanawat.bcr <thanwat.bcr@gmail.com>
2022-11-03 21:04:03 +0700

    fix: error response

--- src/middlewares/qr.js
+++ src/middlewares/qr.js
@@ -6,14 +6,14 @@ const verifyToken = (req, res, next) => {
   let token = req.body.token
 
   if (!token) {
-    return res.status(403).send("A token is required for authentication");
+    return res.status(403).json({ status: "TOKEN_IS_REQUIRED" })
   }
 
   try {
     const decoded = jwt.verify(token, config.TOKEN_KEY);
     req.user = decoded;
   } catch (err) {
-    return res.status(401).send("Invalid Token");
+    return res.status(401).json({ status: "TOKEN_IS_INVALID" })
   }
   return next();
 };
